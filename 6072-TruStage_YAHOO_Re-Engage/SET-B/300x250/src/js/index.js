9eaeb4df2debef90381f8ffe9231cb3a0cd665b6
dq-cs <107273190+dq-cs@users.noreply.github.com>
2023-05-01 06:20:37 -0700

    round 3

--- 6072-TruStage_YAHOO_Re-Engage/SET-B/300x250/src/js/index.js
+++ 6072-TruStage_YAHOO_Re-Engage/SET-B/300x250/src/js/index.js
@@ -130,7 +130,7 @@ var nameSpace = O2KL || {};
 
             .fromTo(
                 ["#img-1"],
-                0.5, { x: "-25%" }, {
+                0.75, { x: "-25%" }, {
                     transformPerspective: 400,
                     autoAlpha: 1,
                     x: 0,
@@ -141,7 +141,7 @@ var nameSpace = O2KL || {};
             )
             .fromTo(
                 ["#logo-bg"],
-                0.5, { x: "-25%" }, {
+                0.75, { x: "-25%" }, {
                     transformPerspective: 400,
                     autoAlpha: 1,
                     x: 0,
